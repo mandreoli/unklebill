a583d255b47a8bd2773fa2ccf0d6fa7f7e7e03f2
michi.andreoli <michi.andreoli@29f713b4-38d8-a555-920c-444384c74762>
2011-01-22 13:45:27 +0000

    corretto bug colorazione trasferimenti nella tabella e inseriti totali nel report

--- UBill/src/executor/CreateReport.java
+++ UBill/src/executor/CreateReport.java
@@ -23,6 +23,10 @@ public class CreateReport {
 	private Color active = new Color(0, 128, 0);
 	private Color neutro = new Color(128, 128, 128);
 	private Color passive = new Color(128, 0, 0);
+	private double entranceTot = 0.0;
+	private double entranceTransf = 0.0;
+	private double exitTot = 0.0;
+	private double exitTransf = 0.0;
 
 	public CreateReport(String filename, String month, String year) {
 		Document document = new Document();
@@ -37,9 +41,11 @@ public class CreateReport {
 		document.open();
 		
 		Font title = new Font(Font.FontFamily.HELVETICA, 18, Font.BOLD);
-		Font subtitle = new Font(Font.FontFamily.HELVETICA, 14, Font.BOLD);		
+		Font header = new Font(Font.FontFamily.HELVETICA, 14, Font.BOLDITALIC);		
 		Font emph = new Font(Font.FontFamily.HELVETICA, 12, Font.ITALIC);
-		Font bold = new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD);
+		Font bold = new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD, new BaseColor(Color.BLACK));
+		Font boldPassive = new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD, new BaseColor(passive));
+		Font boldActive = new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD, new BaseColor(active));
 		Font dataPassive = new Font(Font.FontFamily.HELVETICA, 12, Font.NORMAL, new BaseColor(passive));
 		Font dataActive = new Font(Font.FontFamily.HELVETICA, 12, Font.NORMAL, new BaseColor(active));
 		Font dataNeutro = new Font(Font.FontFamily.HELVETICA, 12, Font.NORMAL, new BaseColor(neutro));
@@ -49,10 +55,10 @@ public class CreateReport {
 		table.setSpacingBefore(14);
 		table.setSpacingAfter(14);
 		
-		PdfPCell c1 = new PdfPCell(new Paragraph(new Chunk("Day", subtitle)));
-		PdfPCell c2 = new PdfPCell(new PdfPCell(new Paragraph(new Chunk("Entrance", subtitle))));		
-		PdfPCell c3 = new PdfPCell(new Paragraph(new Chunk("Exit", subtitle)));
-		PdfPCell c4 = new PdfPCell(new Paragraph(new Chunk("Category", subtitle)));
+		PdfPCell c1 = new PdfPCell(new Paragraph(new Chunk("Day", header)));
+		PdfPCell c2 = new PdfPCell(new PdfPCell(new Paragraph(new Chunk("Entrance", header))));		
+		PdfPCell c3 = new PdfPCell(new Paragraph(new Chunk("Exit", header)));
+		PdfPCell c4 = new PdfPCell(new Paragraph(new Chunk("Category", header)));
 		c1.setBorder(PdfPCell.NO_BORDER);
 		c2.setBorder(PdfPCell.NO_BORDER);
 		c3.setBorder(PdfPCell.NO_BORDER);
@@ -66,29 +72,33 @@ public class CreateReport {
 			if (t.getRefid() == 0) {
 				if (t.getType() == '+') {
 					c1.setPhrase(new Paragraph(new Chunk(Date.getDay(t.getDay()), dataActive)));
-					c2.setPhrase(new Paragraph(new Chunk(String.valueOf(t.getPayment()), dataActive)));
+					c2.setPhrase(new Paragraph(new Chunk("+"+String.valueOf(t.getPayment())+" "+Login.getAccount().getCurrency(), dataActive)));
 					c3.setPhrase(new Paragraph(new Chunk("", dataActive)));
-					c4.setPhrase(new Paragraph(new Chunk(t.getEntry(), dataActive)));					
+					c4.setPhrase(new Paragraph(new Chunk(t.getEntry(), dataActive)));
+					entranceTot += t.getPayment();					
 				}
 				else {
 					c1.setPhrase(new Paragraph(new Chunk(Date.getDay(t.getDay()), dataPassive)));
 					c2.setPhrase(new Paragraph(new Chunk("", dataPassive)));
-					c3.setPhrase(new Paragraph(new Chunk(String.valueOf(t.getPayment()), dataPassive)));
-					c4.setPhrase(new Paragraph(new Chunk(t.getEntry(), dataPassive)));
+					c3.setPhrase(new Paragraph(new Chunk("-"+String.valueOf(t.getPayment())+" "+Login.getAccount().getCurrency(), dataPassive)));
+					c4.setPhrase(new Paragraph(new Chunk(t.getEntry(), dataPassive)));					
+					exitTot += t.getPayment();
 				}
 			}
 			else {
 				if (t.getType() == '+') {
 					c1.setPhrase(new Paragraph(new Chunk(Date.getDay(t.getDay()), dataNeutro)));
-					c2.setPhrase(new Paragraph(new Chunk(String.valueOf(t.getPayment()), dataNeutro)));
+					c2.setPhrase(new Paragraph(new Chunk("+"+String.valueOf(t.getPayment())+" "+Login.getAccount().getCurrency(), dataNeutro)));
 					c3.setPhrase(new Paragraph(new Chunk("", dataNeutro)));
 					c4.setPhrase(new Paragraph(new Chunk(t.getEntry()+" from "+t.getReference(), dataNeutro)));
+					entranceTransf += t.getPayment();
 				}
 				else {
 					c1.setPhrase(new Paragraph(new Chunk(Date.getDay(t.getDay()), dataNeutro)));
 					c2.setPhrase(new Paragraph(new Chunk("", dataNeutro)));
-					c3.setPhrase(new Paragraph(new Chunk(String.valueOf(t.getPayment()), dataNeutro)));
+					c3.setPhrase(new Paragraph(new Chunk("-"+String.valueOf(t.getPayment())+" "+Login.getAccount().getCurrency(), dataNeutro)));
 					c4.setPhrase(new Paragraph(new Chunk(t.getEntry()+" to "+t.getReference(), dataNeutro)));
+					exitTransf += t.getPayment();
 				}
 			}
 			table.addCell(c1);
@@ -96,6 +106,14 @@ public class CreateReport {
 			table.addCell(c3);
 			table.addCell(c4);
 		}
+		c1.setPhrase(new Paragraph(new Chunk("Total", bold)));
+		c2.setPhrase(new Paragraph(new Chunk("+"+String.valueOf(FieldParser.roundDouble(entranceTot))+" "+Login.getAccount().getCurrency(), bold)));
+		c3.setPhrase(new Paragraph(new Chunk("-"+String.valueOf(FieldParser.roundDouble(exitTot))+" "+Login.getAccount().getCurrency(), bold)));
+		c4.setPhrase(new Paragraph(new Chunk("= "+String.valueOf(FieldParser.roundDouble(entranceTot-exitTot))+" "+Login.getAccount().getCurrency(), bold)));
+		table.addCell(c1);
+		table.addCell(c2);
+		table.addCell(c3);
+		table.addCell(c4);
 		
 		try {
 			document.add(new Paragraph(new Chunk("Report of "+Login.getAccount().getAccount()+" for "+month+" "+year, title)));
