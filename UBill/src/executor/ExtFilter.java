1167109a67bd5cc9113c71707758623b15ae0062
michi.andreoli <michi.andreoli@29f713b4-38d8-a555-920c-444384c74762>
2011-01-27 13:07:49 +0000

    aggiunta licenza GPL

--- UBill/src/executor/ExtFilter.java
+++ UBill/src/executor/ExtFilter.java
@@ -1,21 +1,24 @@
-package executor;
 /**
-This file is part of iRAW.
+ * Copyright 2011 Michele Andreoli
+ * 
+ * This file is part of UnkleBill.
+ *
+ * UnkleBill is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * UnkleBill is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with UnkleBill; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ **/
 
-iRAW is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-iRAW is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with iRAW.  If not, see <http://www.gnu.org/licenses/>.
-**/
-
+package executor;
 
 import java.io.File;
 
--- UBill/src/executor/UBill.java
+++ UBill/src/executor/UBill.java
@@ -1,3 +1,23 @@
+/**
+ * Copyright 2011 Michele Andreoli
+ * 
+ * This file is part of UnkleBill.
+ *
+ * UnkleBill is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * UnkleBill is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with UnkleBill; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ **/
+
 package executor;
 
 import java.io.IOException;
@@ -16,7 +36,7 @@ import boundary.SplashScreen;
 public class UBill {
 
 	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {		
-		/*
+		
 		// initialize logging to go to rolling log file
         LogManager logManager = LogManager.getLogManager();
         logManager.reset();
@@ -50,7 +70,7 @@ public class UBill {
 		logger = Logger.getLogger("stderr");
 		los= new LoggingOutputStream(logger, StdOutErrLevel.STDERR);
 		System.setErr(new PrintStream(los, true));
-		*/
+		
 		try {
 			UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
         } catch (Exception e) {
